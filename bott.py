ff225948f96751c0eda1bea608a085c0f9d1b8c4
mCrosso <macrosso2008@gmail.com>
2020-07-05 14:05:53 -0700

    Update bott.py

--- bott.py
+++ bott.py
@@ -3,6 +3,7 @@ import os
 import discord
 
 from discord.ext import commands
+import sqlite3
 
 client = commands.Bot(command_prefix='.')
 
@@ -19,11 +20,9 @@ async def testingrole2(ctx):
     await user.add_roles(role)
 
 
-
 @client.event
 async def on_ready():
     print('Bot running!')
-
 
 @client.command(hidden=True)
 async def load(ctx, extension):
@@ -35,5 +34,6 @@ async def unload(ctx, extension):
 for filename in os.listdir('./cogs'):
     if filename.endswith('.py'):
         client.load_extension(f'cogs.{filename[:-3]}')
+
 
 client.run(os.environ['Discord_Token'])
