15d03cb688111e2e0319c80172dca0d7825c115c
mCrosso <macrosso2008@gmail.com>
2020-07-09 14:32:07 -0700

    Update Images.py

--- cogs/Images.py
+++ cogs/Images.py
@@ -12,36 +12,13 @@ class Images(commands.Cog):
         self.client = client
 
     @commands.command(pass_context=True)
-    async def whois(self, ctx, user: discord.User = None):
-        if user is None:
-            user = ctx.author
-        response = requests.get(user.avatar_url)
-        img = Image.open(BytesIO(response.content))
-        #----------------------------------------------------------------------
-        img_fraction = 0.50
-        W, H = (128, 128)
-        fs = 1
-        txt = f'This is {user.name}.'
-        font_type = ImageFont.truetype('arial.ttf', 30)
-        while font_type.getsize(txt)[0] < img_fraction * img.size[0]:
-            fs += 1
-            font_type = ImageFont.truetype("arial.ttf", fs)
-        # ----------------------------------------------------------------
-        draw = ImageDraw.Draw(img)
-        w, h = draw.textsize(txt)
-        draw.text(((W - w) / 2, (H - h) / 2), text=txt, fill=(255, 255, 255), font=font_type)
-        img = img.convert('RGB')
-        img.save('img.jpg')
-        await ctx.send(file=discord.File('img.jpg'))
-
-    @commands.command(pass_context=True)
     async def blur(self, ctx, user: discord.User = None):
         if user is None:
             user = ctx.author
         response = requests.get(user.avatar_url)
         img = Image.open(BytesIO(response.content))
+        img = img.convert('RGB')
         blurred_image = img.filter(ImageFilter.BLUR)
-        blurred_image = blurred_image.convert('RGB')
         blurred_image.save('img.jpg')
         await ctx.send(file=discord.File('img.jpg'))
         os.remove('img.jpg')
