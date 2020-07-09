f31f0e717930161f82cd81b4eacfe7c8ce91e375
mCrosso <macrosso2008@gmail.com>
2020-07-09 12:25:35 -0700

    Update Images.py

--- cogs/Images.py
+++ cogs/Images.py
@@ -7,7 +7,6 @@ from discord.ext import commands
 from discord.ext.commands import bot
 import os
 
-
 class Images(commands.Cog):
     def __init__(self, client):
         self.client = client
@@ -18,6 +17,7 @@ class Images(commands.Cog):
             user = ctx.author
         response = requests.get(user.avatar_url)
         img = Image.open(BytesIO(response.content))
+        #----------------------------------------------------------------------
         img_fraction = 0.50
         W, H = (128, 128)
         fs = 1
@@ -26,6 +26,7 @@ class Images(commands.Cog):
         while font_type.getsize(txt)[0] < img_fraction * img.size[0]:
             fs += 1
             font_type = ImageFont.truetype("arial.ttf", fs)
+        # ----------------------------------------------------------------
         draw = ImageDraw.Draw(img)
         w, h = draw.textsize(txt)
         draw.text(((W - w) / 2, (H - h) / 2), text=txt, fill=(255, 255, 255), font=font_type)
